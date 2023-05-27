4de379714859b4d455895bb922651795785125a2
solid3032 <134340291+solid3032@users.noreply.github.com>
2023-05-27 19:28:18 -0300

    Add files via upload

---
+++ Menu Hamburguer Horinzontal.js
@@ -0,0 +1,12 @@
+function abreMenu()  /*Alterar a altura e a cor ao abrir*/
+{
+  document.getElementById("MenuMobile").style.height  = "100%";
+
+}
+
+function fechaMenu()  /*Alterar a altura e a cor ao fechar*/
+{
+  document.getElementById("MenuMobile").style.height = "0%";
+
+  
+}
